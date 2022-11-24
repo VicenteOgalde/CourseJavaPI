447f0ee4c222a0589f0bd27a5b95f0ec0f50398e
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-24 11:59:02 -0300

    new content added

---
+++ Examples/src/OOP/Car.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Car {
+	
+	int wheels;
+	int length;
+	int width;
+	int motor;
+	int weight;
+	
+	public Car(int wheels, int length, int width, int motor, int weight) {
+		this.wheels=wheels;
+		this.length=length;
+		this.width=width;
+		this.motor=motor;
+		this.weight=weight;
+	}
+
+}
---
+++ Examples/src/OOP/CarImpl.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class CarImpl {
+
+	public static void main(String[] args) {
+		
+		Car car = new Car(4,4,2,1600,2200);
+		System.out.println("this car have "+car.wheels);
+		
+
+	}
+
+}
