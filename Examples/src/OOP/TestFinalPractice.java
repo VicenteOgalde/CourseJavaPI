fcaff9576e9a1c8f5d0e3728c9dfe49cb6f1e9d1
vicente_ogalde <vicenteogalde@gmail.com>
2022-11-29 13:33:07 -0300

    practice n37

--- Examples/src/OOP/TestFinalPractice.java
+++ Examples/src/OOP/TestFinalPractice.java
@@ -8,8 +8,10 @@ public class TestFinalPractice {
 		EmployeeTest emp2= new EmployeeTest("moon");
 		
 		emp1.setSection("RRHH");
-		System.out.println("Employee: "+emp2.getName()+" work in the section: "+emp2.getSection());
-		System.out.println("Employee: "+emp1.getName()+" work in the section: "+emp1.getSection());
+		System.out.println("Employee: "+emp2.getName()+
+				" work in the section: "+emp2.getSection()+" with id: "+emp1.getId());
+		System.out.println("Employee: "+emp1.getName()+
+				" work in the section: "+emp1.getSection()+" with id: "+emp2.getId());
 		
 
 	}
@@ -18,9 +20,13 @@ public class TestFinalPractice {
 class EmployeeTest{
 	private final String name;
 	private String section;
+	private int id;
+	private static int nextId=1;//static only work for the class not the objects you create
 	public EmployeeTest(String name) {
 		this.name=name;
 		this.section="adm";
+		this.id=this.nextId;
+		this.nextId++;
 	}
 	public void setSection(String section) {
 		this.section=section;
@@ -30,6 +36,9 @@ class EmployeeTest{
 	}
 	public String getName() {
 		return this.name;
+	}
+	public int getId() {
+		return this.id;
 	}
 
 }
