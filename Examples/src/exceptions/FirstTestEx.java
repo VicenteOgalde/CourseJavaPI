8c5ce6afe287d9ba5cabd777fa920e60f21d39d6
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-02 20:10:08 -0300

    practice n145

--- Examples/src/exceptions/FirstTestEx.java
+++ Examples/src/exceptions/FirstTestEx.java
@@ -7,26 +7,32 @@ public class FirstTestEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		inputData();
-		
-		}
-		
-		static void inputData() throws InputMismatchException {
+		System.out.println("run the program type 1 \nexit the program type 2");
+		Scanner sc = new Scanner(System.in);
+		int choice = sc.nextInt();
+		if (choice == 1) {
 			try {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("type your name: ");
-			String userName=sc.nextLine();
-			System.out.println("type your age: ");
-			int age = sc.nextInt();
-			System.out.println("your name is: "+userName+
-					" and your age is: "+age);
-			sc.close();
-			}catch(InputMismatchException e) {
+				inputData();
+			} catch (InputMismatchException e) {
 				System.out.println("age incorrect");
 			}
+		} else {
+			System.out.println("bye");
+			System.exit(0);
 		}
+		sc.close();
+	}
+
+	static void inputData() throws InputMismatchException {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("type your name: ");
+		String userName = sc.nextLine();
+		System.out.println("type your age: ");
+		int age = sc.nextInt();
+		System.out.println("your name is: " + userName + " and your age is: " + age);
+		sc.close();
 
 	}
 
-
+}
