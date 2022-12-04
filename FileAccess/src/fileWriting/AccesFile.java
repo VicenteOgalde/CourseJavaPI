a8111ce67584b97b34244f26d2cc85a4a1312112
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-04 13:06:48 -0300

    practice n152

---
+++ FileAccess/src/fileWriting/AccesFile.java
@@ -0,0 +1,34 @@
+package fileWriting;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AccesFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReadFile readFile = new ReadFile();
+		readFile.read();
+	}
+
+}
+
+class ReadFile{
+	
+	public void read() {
+		try {
+			FileReader dataIn= new FileReader("D:/Proyectos JAVA/CursoPildorasInformaticas/Readme.txt");
+			int c=0;
+			while(c!=-1) {
+				c=dataIn.read();
+				char letter=(char)c;
+				System.out.print(letter);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File Not Found");
+			e.printStackTrace();
+		}
+	}
+}
