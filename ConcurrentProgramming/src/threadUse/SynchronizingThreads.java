b9e409d9b01d7906e697dffcc3fa140867e4453c
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-06 22:41:57 -0300

    practice n173

--- ConcurrentProgramming/src/threadUse/SynchronizingThreads.java
+++ ConcurrentProgramming/src/threadUse/SynchronizingThreads.java
@@ -5,10 +5,15 @@ public class SynchronizingThreads {
 	public static void main(String[] args) {
 		
 		MultipleThreads thread1= new MultipleThreads();
-		MultipleThreads thread2= new MultipleThreads();
+		MultipleThreads2 thread2= new MultipleThreads2(thread1);
+		
+		thread2.start();
 		
 		thread1.start();
 		
+		
+		
+		/*
 		try {
 			thread1.join();//forces to finish all tasks in this thread first
 		} catch (InterruptedException e) {
@@ -17,6 +22,14 @@ public class SynchronizingThreads {
 		}
 		
 		thread2.start();
+		
+		try {
+			thread2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		System.out.println("all task finish");
 
 	}
 
@@ -25,6 +38,29 @@ public class SynchronizingThreads {
 class MultipleThreads extends Thread{
 	
 	public void run(){
+		for(int i=0;i<15;i++) {
+			System.out.println("execute thread "+this.getName());
+		}
+	}
+	
+}
+class MultipleThreads2 extends Thread{
+	
+	private Thread thread;
+	
+	public MultipleThreads2(Thread t) {
+		this.thread=t;
+	}
+	
+	public void run(){
+		
+		try {
+			thread.join();//now you need to wait for the first thread to finish
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		for(int i=0;i<15;i++) {
 			System.out.println("execute thread "+this.getName());
 		}
