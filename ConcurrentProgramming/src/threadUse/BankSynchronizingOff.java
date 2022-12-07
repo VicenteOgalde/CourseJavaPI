6ac06b1766409f3fbcd52ec7ab13f6ff4ececfcd
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-07 12:02:39 -0300

    practice n174

--- ConcurrentProgramming/src/threadUse/BankSynchronizingOff.java
+++ ConcurrentProgramming/src/threadUse/BankSynchronizingOff.java
@@ -3,8 +3,17 @@ package threadUse;
 public class BankSynchronizingOff {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Bank b= new Bank();
+		
+		for(int i=0;i<100;i++) {
+			
+			TransferExecution r = new TransferExecution(b, i, 2000);
+			Thread t = new Thread(r);
+			t.start();
+			
+		}
+		
 	}
 
 }
@@ -15,8 +24,8 @@ class Bank{
 	
 	public Bank () {
 		accounts= new double[100];
-		for(double d: accounts) {
-			d=2000;
+		for(int i=0;i<accounts.length;i++) {
+			accounts[i]=2000;
 		}
 	}
 	public void transfer(int origin,int destiny,double amount) {
@@ -24,9 +33,10 @@ class Bank{
 			return;
 		}
 		System.out.println(Thread.currentThread());
-		accounts[origin]=accounts[origin]-amount;
-		System.out.printf("%10.2 of %d for %d",amount,origin,destiny);
-		accounts[destiny]=accounts[destiny]+amount;
+		accounts[origin]-=amount;
+		System.out.printf(" %10.2f of %d for %d ",amount,origin,destiny);
+		accounts[destiny]+=amount;
+		System.out.printf(" Total Balance: %10.2f%n",getTotalBalance());
 	}
 	public double getTotalBalance() {
 		double totalBalance=0;
@@ -37,3 +47,45 @@ class Bank{
 	}
 	
 }
+
+class TransferExecution implements Runnable{
+
+	private Bank bank;
+	private int account;
+	private double maxTransf;
+	
+	public TransferExecution(Bank b,int acc,double max) {
+		
+		this.bank=b;
+		this.account=acc;
+		this.maxTransf=max;
+		
+	}
+	
+	@Override
+	public void run() {
+		
+		try {
+		while(true) {
+			int destinyAcc=(int)(100*Math.random());
+			double amountTf=(maxTransf*Math.random());
+			bank.transfer(account, destinyAcc, amountTf);
+			
+			Thread.sleep((int)(Math.random()*10));
+		}
+		}catch(InterruptedException e) {
+			
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
