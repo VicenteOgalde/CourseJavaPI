5a0e3c093ec7c20c32d00ee0d71611eeb9fe1113
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-08 12:16:57 -0300

    practice n187

--- Collections/src/collectionPractice/TreeSetTest.java
+++ Collections/src/collectionPractice/TreeSetTest.java
@@ -1,5 +1,6 @@
 package collectionPractice;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class TreeSetTest {
@@ -18,10 +19,10 @@ public class TreeSetTest {
 		}
 		*/
 		Items first= new Items(1, "number 1");
-		Items second= new Items(2, "number 2");
-		Items third= new Items(3, "number 3");
+		Items second= new Items(2, "znumber 2");
+		Items third= new Items(3, "anumber 3");
 		
-		TreeSet<Items> items= new TreeSet<Items>();
+		TreeSet<Items> items= new TreeSet<Items>();//using comparable for sort using compareTo method
 		
 		items.add(second);
 		items.add(first);
@@ -30,15 +31,30 @@ public class TreeSetTest {
 		for (Items item : items) {
 			System.out.println(item.getDescription());
 		}
+		
+		Items comparator = new Items();
+		
+		TreeSet<Items> sortItems= new TreeSet<Items>(comparator);//use interfaz comparator for sort with string
+		
+		sortItems.addAll(items);
+		
+		for (Items item : sortItems) {
+			System.out.println(item.getDescription());
+		}
 
 	}
 
 }
 
-class Items implements Comparable<Items> {
+class Items implements Comparable<Items>,Comparator<Items> {
 
 	private int id;
 	private String description;
+	
+
+	public Items() {
+		
+	}
 
 	public Items(int id, String description) {
 		this.id = id;
@@ -65,6 +81,15 @@ class Items implements Comparable<Items> {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int compare(Items o1, Items o2) {
+		
+		String descriptionA=o1.getDescription();
+		String descriptionB=o2.getDescription();
+		
+		return descriptionA.compareTo(descriptionB);
 	}
 
 }
