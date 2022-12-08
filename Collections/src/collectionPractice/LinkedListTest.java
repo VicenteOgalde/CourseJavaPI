629683f2ef7a443e8bd09be514cf42b81b227e84
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-08 11:36:11 -0300

    practice n185

--- Collections/src/collectionPractice/LinkedListTest.java
+++ Collections/src/collectionPractice/LinkedListTest.java
@@ -6,7 +6,7 @@ import java.util.ListIterator;
 public class LinkedListTest {
 
 	public static void main(String[] args) {
-		
+		/*
 		LinkedList <String> persons = new LinkedList<>();
 		persons.add("ana");
 		persons.add("sun");
@@ -20,7 +20,48 @@ public class LinkedListTest {
 		for(String i:persons) {
 			System.out.println(i);
 		}
+		*/
 		
+		LinkedList<String> countries= new LinkedList<String>();
+		
+		countries.add("Spain");
+		countries.add("Colombia");
+		countries.add("Mexico");
+		countries.add("Peru");
+		
+		LinkedList<String> capitals= new LinkedList<String>();
+		
+		capitals.add("Madrid");
+		capitals.add("Bogota");
+		capitals.add("Mexico City");
+		capitals.add("Lima");
+		
+		ListIterator<String> iterCountries= countries.listIterator();
+		ListIterator<String> iterCapitals=capitals.listIterator();
+		
+		while(iterCapitals.hasNext()) {//add between position
+			if(iterCountries.hasNext()) {
+				iterCountries.next();
+			}
+			iterCountries.add(iterCapitals.next());
+		}
+		System.out.println(countries);
+		
+		iterCapitals=capitals.listIterator();
+		
+		while(iterCapitals.hasNext()) {//remove pair items position
+			iterCapitals.next();
+			if(iterCapitals.hasNext()) {
+				iterCapitals.next();
+				iterCapitals.remove();
+			}
+			
+		}
+		System.out.println(capitals);
+		
+		countries.removeAll(capitals);// remove linkedlist capitals from countries
+		
+		System.out.println(countries);
 	}
 
 }
