0a4ced3f9b4af12cc4ae045aaba78064f280b106
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-11 13:30:24 -0300

    practice n206

---
+++ JDBC_BD/src/bdConexion/QueryApp.java
@@ -0,0 +1,86 @@
+package bdConexion;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class QueryApp {
+
+	public static void main(String[] args) {
+		
+		FrameApp meFrame =new FrameApp();
+		meFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		meFrame.setVisible(true);
+		
+	}
+	
+
+}
+class FrameApp extends JFrame{
+	
+	private JComboBox region,comuna;
+	private JTextArea result;
+	
+	public FrameApp() {
+		
+		setTitle("Query DB");
+		setBounds(500, 300, 400, 400);
+		setLayout(new BorderLayout());
+		JPanel menu=new JPanel();
+		menu.setLayout(new FlowLayout());
+		region=new JComboBox();
+		region.setEditable(false);
+		region.addItem("All");
+		comuna= new JComboBox();
+		comuna.setEditable(false);
+		comuna.addItem("All");
+		result=new JTextArea();
+		result.setEditable(false);
+		add(result);
+		menu.add(region);
+		menu.add(comuna);
+		add(menu,BorderLayout.NORTH);
+		add(result,BorderLayout.CENTER);
+		JButton buttonQuery= new JButton("Query");
+		add(buttonQuery,BorderLayout.SOUTH);
+		
+		
+		try {
+			//1.create connection
+			Connection meConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/persona",
+					"root","");
+			//2. create statement
+			Statement meStatement = meConnection.createStatement();
+			//3.execute sql
+			
+			ResultSet meResultSet=meStatement.executeQuery("SELECT DISTINCTROW comuna FROM persona");
+			//read the resultset
+			while(meResultSet.next()) {
+				comuna.addItem(meResultSet.getString(1));
+			}
+			meResultSet.close();
+			
+			meResultSet=meStatement.executeQuery("SELECT DISTINCTROW region FROM persona");
+			while(meResultSet.next()) {
+				region.addItem(meResultSet.getString(1));
+			}
+			meResultSet.close();
+			
+			
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		
+	}
+}
