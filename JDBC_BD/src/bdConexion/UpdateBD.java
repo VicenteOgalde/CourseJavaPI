ce8c29d1d49a47d42be4dc400ca91bf7985a4bf9
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-11 11:26:58 -0300

    practice n204

---
+++ JDBC_BD/src/bdConexion/UpdateBD.java
@@ -0,0 +1,46 @@
+package bdConexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UpdateBD {
+
+	public static void main(String[] args) {
+		
+		try {
+			//1.create connection
+			Connection meConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/curso",
+					"root","");
+			//2. create statement
+			Statement meStatement = meConnection.createStatement();
+			
+			/*//Insert into db
+			String instructionSQL= "INSERT INTO USUARIOS "
+					+ "(id,nombre,apellido,telefono)"
+					+" VALUES ('256','sun','moon','56+asd123')";
+			*/
+			
+			/* //update name
+			String instructionSQL= "UPDATE USUARIOS SET nombre='greg' WHERE "
+					+ "id = '256' ";
+			*/
+			
+			 //Delete 
+			String instructionSQL= "DELETE FROM USUARIOS WHERE "
+					+ "nombre='greg'";
+			
+			meStatement.executeUpdate(instructionSQL);
+			
+			
+		
+			
+			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
