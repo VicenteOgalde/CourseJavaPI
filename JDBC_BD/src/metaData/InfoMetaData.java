5aae8be0e620701c90069572953b64439f42d6e4
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-13 12:13:35 -0300

    practice n223

--- JDBC_BD/src/metaData/InfoMetaData.java
+++ JDBC_BD/src/metaData/InfoMetaData.java
@@ -3,13 +3,15 @@ package metaData;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class InfoMetaData {
 
 	public static void main(String[] args) {
 		
-		showInfoMetaData();
+		//showInfoMetaData();
+		showInfoTable();
 
 	}
 	
@@ -40,6 +42,59 @@ public class InfoMetaData {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	
+	static void showInfoTable() {
+		
+	Connection conn=null;
+	ResultSet rs=null;
+		
+		try {
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/bootcamp",
+					"root","");
+			
+			DatabaseMetaData bd= conn.getMetaData();
+			
+			System.out.println("Tables");
+			System.out.println("-----------------------");
+			
+			rs=bd.getTables(null, null, "c%", null);
+			
+			while (rs.next()) {
+				
+				System.out.println(rs.getString("TABLE_NAME"));
+				
+			}
+			System.out.println();
+			System.out.println("columns");
+			System.out.println("--------------");
+			rs=bd.getColumns(null, null, "comuna", null);
+			
+			while (rs.next()) {
+				
+				System.out.println(rs.getString("COLUMN_NAME"));
+				
+			}
+			
+			
+			
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
 	}
 
 }
