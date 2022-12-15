ae26e526949a334fbeeeb32b7b752e7cab339e21
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-15 18:52:11 -0300

    practice n247

---
+++ Servlets/src/main/java/com/test/servlet/UserRegister.java
@@ -0,0 +1,52 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserRegister
+ */
+@WebServlet("/UserRegister")
+public class UserRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter output= response.getWriter();
+		
+		output.println("<html><body> <h1>Form</h1>");
+		output.println("");
+		output.println("Name is :"+request.getParameter("name"));
+		output.println("</br>");
+		output.println("Date is: "+new Date());
+		output.println("</body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
