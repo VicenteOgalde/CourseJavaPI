314b97b39b00a8297685aaae77e8a8e96888ba15
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-15 17:00:02 -0300

    practice n245

---
+++ Servlets/src/main/java/com/test/servlet/ServletTest.java
@@ -0,0 +1,52 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTest
+ */
+@WebServlet("/ServletTest")
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter output= response.getWriter();
+		
+		output.println("<html><body> <h1> Test Servlet</h1>");
+		output.println("");
+		output.println("");
+		output.println("");
+		output.println("Date is: "+new Date());
+		output.println("</body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
