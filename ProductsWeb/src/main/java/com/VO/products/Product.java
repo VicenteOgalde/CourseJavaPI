29d429d7c91a1d29dc8077b7cf8cc801d4927f69
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-16 11:47:37 -0300

    practice n251

---
+++ ProductsWeb/src/main/java/com/VO/products/Product.java
@@ -0,0 +1,63 @@
+package com.VO.products;
+
+import java.util.Date;
+
+public class Product {
+	
+	private String id;
+	private String  name;
+	private String section;
+	private Double price;
+	private Date date;
+	public Product(String id, String name, String section, Double price, Date date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.section = section;
+		this.price = price;
+		this.date = date;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSection() {
+		return section;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "Product Id=" + getId() + ",Name=" + getName() + ", Section=" + getSection()
+				+ ", Price=" + getPrice() + ", Date=" + getDate();
+	}
+	
+	
+	
+
+}
---
+++ ProductsWeb/src/main/java/com/VO/products/ProductRepository.java
@@ -0,0 +1,37 @@
+package com.VO.products;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+public class ProductRepository {
+
+	private DataSource dataOrigin;
+	
+	public ProductRepository(DataSource dataOrigin) {
+		this.dataOrigin=dataOrigin;
+	}
+	
+	public List<Product> getProducts()throws Exception{
+		
+		List<Product> products= new ArrayList<Product>();
+		Connection conn=null;
+		Statement st=null;
+		ResultSet rs=null;
+		
+		conn=dataOrigin.getConnection();
+		st=conn.createStatement();
+		String sql="Select * from product";
+		rs=st.executeQuery(sql);
+		while(rs.next()) {
+			products.add(new Product(rs.getString("id"),rs.getString("name"),rs.getString("section"),
+					rs.getDouble("price"),rs.getDate("date")));
+		}
+		return products;
+	}
+	
+}
