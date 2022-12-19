97c5316f373730ae02d8237aee1f4d2a377cd572
vicente_ogalde <vicenteogalde@gmail.com>
2022-12-19 14:37:52 -0300

    practice n272

---
+++ Extras/src/instrospection/TestInstrospection.java
@@ -0,0 +1,51 @@
+package instrospection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.Scanner;
+
+public class TestInstrospection {
+
+	public static void main(String[] args) {
+		
+		Scanner input= new Scanner(System.in);
+		System.out.println("Type the class name : ");
+		String nameC= input.next();
+		
+		
+		try {
+			Class cl = Class.forName(nameC);
+			Class superCl=cl.getSuperclass();
+			System.out.println("Class is: "+cl);
+			
+			if(cl!=null && superCl!=Object.class) {
+				System.out.println("extends from "+superCl.getName());
+			}
+			contrucPrint(cl);
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+
+	}
+	
+	public static void contrucPrint(Class cl) {
+		Constructor[] consArr=cl.getConstructors();
+		
+		for (Constructor c : consArr) {
+			System.out.print(" "+Modifier.toString(c.getModifiers()));
+			System.out.print(" "+c.getName()+"(");
+			Class [] params=c.getParameterTypes();
+			for (int i=0;i<params.length;i++) {
+				if(i>0)System.out.print(", ");
+				System.out.println(params[i].getName());
+			}
+			System.out.print(");");
+		}
+		
+	}
+
+}
